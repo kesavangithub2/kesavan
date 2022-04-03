29fa1991649c30305e4036a1504e970d9acf2a66
kesavangithub2 <910618104045@klnce.edu>
2022-04-03 20:43:27 +0530

    first commit

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,7 @@ const Home = () => {
     return (
         <div>
             <div className="icon-bar">
-  <a className="active" href="#"><i className="fa fa-home"></i></a> 
-  <a href="#"><i className="fa fa-search"></i></a> 
-  <a href="#"><i className="fa fa-envelope"></i></a> 
-  <a href="#"><i className="fa fa-globe"></i></a>
-  <a href="#"><i className="fa fa-trash"></i></a> 
-</div>
-
+  </div>
         </div>
     );
 }

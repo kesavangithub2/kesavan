97012b430fe3190aa8a6b3bac26560da05599e35
kesavangithub2 <kesavanbakkiya@gmail.com>
2022-04-10 20:42:11 +0530

    first commit

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,9 +2,13 @@ import React from 'react'
 import './css/Home.css';
 
 export default function Home() {
+  const [showResults, setShowResults] = React.useState(false)
+  const onClick = () => setShowResults( showResults ?  false : true)
+
   return (
     <div>
    <div className='header'>
+<div className='hdesk1'>
 <div className='hdesk'>
     <y1> Home</y1>
     <y>Project</y>
@@ -12,13 +16,33 @@ export default function Home() {
 <y>About me</y>
 <y>Contact</y>
 </div>
+</div>
 
 <div className='hmobile'>
+<i class="barm  fa-solid fa-bars fa-2xl" type="submit"  onClick={onClick} />
+      { showResults ? <Results /> : null }
 
+   </div>
 
 </div>
-   </div>
 
     </div>
   )
 }
+
+
+
+
+
+
+
+const Results = () => (
+  <div id="results" className=" mdr">
+ 
+ <p><i class="fa-solid fa-house"></i>  Home </p>
+ 
+ 
+ 
+ 
+  </div>
+)

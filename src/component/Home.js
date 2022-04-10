e551b6ce14ee82a25a4b41ba45fd040731c32e29
Kesavangithub2 <95561472+kesavangithub2@users.noreply.github.com>
2022-04-10 19:13:45 +0530

    Create Home.js

---
+++ src/component/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './css/Home.css';
+
+export default function Home() {
+  return (
+    <div>
+   <div className='header'>
+<div className='hdesk'>
+    <y> Home</y>
+    <y>Project</y>
+
+
+</div>
+
+
+   </div>
+
+    </div>
+  )
+}
